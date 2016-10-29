c14408736ae8322003d0d795fcc758f971a9faeb
vanele23 <vane-leguizamon@hotmail.com>
2016-10-28 23:08:44 -0300

    main.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,16 +7,16 @@
  * # GifCtrl
  * Controller of the miniApp
  */
+var username = '';
+var password = '';
+
+
 angular.module('miniApp')
-  .controller('MainCtrl', function ($http,$scope,$q) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-    $scope.username = "juanry";
-    $scope.password = "Noseyo+16.";
+  .controller('MainCtrl', function($http,$scope,$q, $location,Datos) {
+    $scope.username = Datos.username;
+    $scope.password = Datos.password;
     $scope.change = function(){
+      console.log($scope.username);
       if($scope.user !== undefined) {
         $scope.fetch();
         $scope.fetchRepositories();
@@ -34,18 +34,12 @@ angular.module('miniApp')
         };
         $http.get('https://api.github.com/users/'+$scope.user,config)
         .then(function successCallback(response) {
-          alert("algo que mostrar");
           deferred.resolve($scope.gifs = angular.fromJson(response.data));
         }, function errorCallback(error) {
           console.log(error);
           deferred.resolve($scope.gifs = angular.fromJson({}));
         });
         return deferred.promise;
-    		/*$http.get('//api.giphy.com/v1/gifs/' +
-              $scope.method +
-              '?api_key=dc6zaTOxFJmzC&' +
-              'q=' + $scope.search +
-              '&limit=' + $scope.limit )*/
   	};
     $scope.fetchRepositories = function(){
         var deferred = $q.defer();
@@ -64,10 +58,26 @@ angular.module('miniApp')
           deferred.resolve($scope.gifs = angular.fromJson({}));
         });
         return deferred.promise;
-        /*$http.get('//api.giphy.com/v1/gifs/' +
-              $scope.method +
-              '?api_key=dc6zaTOxFJmzC&' +
-              'q=' + $scope.search +
-              '&limit=' + $scope.limit )*/
+    };
+    $scope.login = function(){
+      var deferred = $q.defer();
+        var authToken = "Basic " + btoa($scope.username + ":" + $scope.password);
+        var config = {
+          headers: {
+            'Authorization': authToken,
+            'Accept': 'application/json;odata=verbose'
+          }
+        };
+        $http.get('https://api.github.com/user',config)
+        .then(function successCallback(response){
+          Datos.username = $scope.username;
+          Datos.password = $scope.password;
+          $location.path('/index');
+        }, function errorCallback(error) {
+            console.log(error);
+           alert("Crendeciales Incorrectas");
+           $location.path('/');
+        });
+        return deferred.promise;
     };
 });
