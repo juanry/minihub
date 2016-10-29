e91f3ea41179642a21c9acce63f4ebb53637e655
vanele23 <vane-leguizamon@hotmail.com>
2016-10-28 22:57:47 -0300

    controller app

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,40 +1,84 @@
 'use strict';
 
 /**
- * @ngdoc overview
- * @name miniApp
+ * @ngdoc function
+ * @name miniApp.controller:GifCtrl
  * @description
- * # miniApp
- *
- * Main module of the application.
+ * # GifCtrl
+ * Controller of the miniApp
  */
-angular
-  .module('miniApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .when('/gif', {
-        templateUrl: 'views/gif.html',
-        controller: 'GifCtrl',
-        controllerAs: 'gif'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+var username = '';
+var password = '';
+
+
+angular.module('miniApp')
+  .controller('MainCtrl', function($http,$scope,$q, $location,Datos) {
+    $scope.username = Datos.username;
+    $scope.password = Datos.password;
+    $scope.change = function(){
+      console.log($scope.username);
+      if($scope.user !== undefined) {
+        $scope.fetch();
+        $scope.fetchRepositories();
+  		}
+	   };
+     
+  	$scope.fetch = function(){
+    		var deferred = $q.defer();
+        var authToken = "Basic " + btoa($scope.username + ":" + $scope.password);
+        var config = {
+          headers: {
+            'Authorization': authToken,
+            'Accept': 'application/json;odata=verbose'
+          }
+        };
+        $http.get('https://api.github.com/users/'+$scope.user,config)
+        .then(function successCallback(response) {
+          deferred.resolve($scope.gifs = angular.fromJson(response.data));
+        }, function errorCallback(error) {
+          console.log(error);
+          deferred.resolve($scope.gifs = angular.fromJson({}));
+        });
+        return deferred.promise;
+  	};
+    $scope.fetchRepositories = function(){
+        var deferred = $q.defer();
+        var authToken = "Basic " + btoa($scope.username + ":" + $scope.password);
+        var config = {
+          headers: {
+            'Authorization': authToken,
+            'Accept': 'application/json;odata=verbose'
+          }
+        };
+        $http.get('https://api.github.com/users/'+$scope.user+'/repos',config)
+        .then(function successCallback(response) {
+          deferred.resolve($scope.repositorios= angular.fromJson(response.data));
+        }, function errorCallback(error) {
+          console.log(error);
+          deferred.resolve($scope.gifs = angular.fromJson({}));
+        });
+        return deferred.promise;
+    };
+    $scope.login = function(){
+      var deferred = $q.defer();
+        var authToken = "Basic " + btoa($scope.username + ":" + $scope.password);
+        var config = {
+          headers: {
+            'Authorization': authToken,
+            'Accept': 'application/json;odata=verbose'
+          }
+        };
+        $http.get('https://api.github.com/user',config)
+        .then(function successCallback(response){
+          Datos.username = $scope.username;
+          Datos.password = $scope.password;
+          $location.path('/index');
+        }, function errorCallback(error) {
+            console.log(error);
+           alert("Crendeciales Incorrectas");
+           $location.path('/');
+        });
+        return deferred.promise;
+    };
+});
+
